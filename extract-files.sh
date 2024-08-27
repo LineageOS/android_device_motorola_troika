6d8616ec767d5e5ed9cac7d4cbd23352e0c06563
Michael Bestas <mkbestas@lineageos.org>
2024-08-27 22:51:30 +0300

    troika: Sync extract scripts with templates
    Change-Id: I98a36854c5a0c0390592911bb2558b34eff77efc

--- extract-files.sh
+++ extract-files.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 function blob_fixup() {
     case "${1}" in
-         # Missing libutils symbols
-         vendor/lib*/sensors.chub.so|vendor/lib*/hw/sensors.troika_sprout.so)
-             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
-             ;;
-         # Remove libhidltransport/libhwbinder dependencies
-         vendor/lib*/libril_sitril.so)
-             "${PATCHELF}" --remove-needed "libhidltransport.so" "${2}"
-             "${PATCHELF}" --remove-needed "libhwbinder.so" "${2}"
-             ;;
+        # Missing libutils symbols
+        vendor/lib*/sensors.chub.so|vendor/lib*/hw/sensors.troika_sprout.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
+            ;;
+        # Remove libhidltransport/libhwbinder dependencies
+        vendor/lib*/libril_sitril.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --remove-needed "libhidltransport.so" "${2}"
+            "${PATCHELF}" --remove-needed "libhwbinder.so" "${2}"
+            ;;
+        *)
+            return 1
+            ;;
     esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
 }
 
 # If we're being sourced by the common script that we called,
