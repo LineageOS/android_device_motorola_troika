ff252312805d6b9c4dc4dd6f6d596ea707633877
Nolen Johnson <johnsonnolen@gmail.com>
2024-11-26 23:30:28 -0500

    troika: ruff check --fix
    Change-Id: I46cf7d87daeac23d311d3e5aabc0442fbae49662

--- extract-files.py
+++ extract-files.py
@@ -12,7 +12,6 @@ from extract_utils.fixups_blob import (
 from extract_utils.fixups_lib import (
     lib_fixup_remove,
     lib_fixup_vendorcompat,
-    lib_fixups,
     lib_fixups_user_type,
     libs_proto_3_9_1,
 )
