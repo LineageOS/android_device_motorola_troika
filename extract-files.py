b46f0e39042f7bf2d2fd56afd5b23e1992365a28
Nolen Johnson <johnsonnolen@gmail.com>
2025-02-02 19:16:20 +0000

    Revert^2 "troika: Enable ELF checks on camera libs"
    This reverts commit c73c2fd3e160f4016af90ced4c99c3c8a3cf85a0.
    
    Reason for revert: Properly converted to BP now.
    
    Change-Id: I280051bb1280f534efba838a9419444e64123425

--- extract-files.py
+++ extract-files.py
@@ -10,7 +10,6 @@ from extract_utils.fixups_blob import (
 )
 
 from extract_utils.fixups_lib import (
-    lib_fixup_remove,
     lib_fixup_vendorcompat,
     lib_fixups_user_type,
     libs_proto_3_9_1,
@@ -28,13 +27,8 @@ namespace_imports = [
     'hardware/samsung_slsi-linaro/graphics',
 ]
 
-libs_remove = (
-     'libGrallocWrapper',
-)
-
 lib_fixups: lib_fixups_user_type = {
     libs_proto_3_9_1: lib_fixup_vendorcompat,
-    libs_remove: lib_fixup_remove,
 }
 
 blob_fixups: blob_fixups_user_type = {
