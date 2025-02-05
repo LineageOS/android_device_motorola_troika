1a4d37e2ec65699333a27ca93a8d17ac009dafd4
Jan Altensen <info@stricted.net>
2025-02-06 00:41:26 +0100

    troika: build camera hal from source
    Change-Id: I7f461a3c97f2a614a4d5e8f13ad89fd08e675728

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,6 +24,13 @@ DEVICE_PATH := device/motorola/troika
 ## Assert
 TARGET_OTA_ASSERT_DEVICE := troika,one_action
 
+# Camera
+$(call soong_config_set,exynos_camera,front_camera_sensor,SENSOR_NAME_OV12A10FF)
+$(call soong_config_set,exynos_camera,back_camera_sensor,SENSOR_NAME_OV12A10)
+$(call soong_config_set,exynos_camera,back_1_camera_sensor,SENSOR_NAME_S5K5E9)
+$(call soong_config_set,exynos_camera,back_2_camera_sensor,SENSOR_NAME_OV16885C)
+$(call soong_config_set,exynos_camera,target_variant,troika)
+
 ## Kernel
 TARGET_KERNEL_CONFIG := troika_defconfig
 
