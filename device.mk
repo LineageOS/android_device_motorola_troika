53d31fa2590f0ce34fa57cd41dd81f3d5300a083
Nolen Johnson <johnsonnolen@gmail.com>
2026-05-21 01:46:05 -0400

    troika: Move soong_config_set{,_bool}'s to product.mk
    * Allowing these in BoardConfig*.mk is a LineageOS addition,
      it is unecessary to store these here.
    * Re-sort other ones while here and seperate out bool entries.
    
    Change-Id: I29c8348cda82ba0890b6db3819c9ea4d7f2fac89

--- device.mk
+++ device.mk
@@ -9,6 +9,13 @@ $(call inherit-product, device/motorola/exynos9610-common/common.mk)
 # Inherit proprietary files
 $(call inherit-product, vendor/motorola/troika/troika-vendor.mk)
 
+# Camera
+$(call soong_config_set,exynos_camera,back_1_camera_sensor,SENSOR_NAME_S5K5E9)
+$(call soong_config_set,exynos_camera,back_2_camera_sensor,SENSOR_NAME_OV16885C)
+$(call soong_config_set,exynos_camera,back_camera_sensor,SENSOR_NAME_OV12A10)
+$(call soong_config_set,exynos_camera,front_camera_sensor,SENSOR_NAME_OV12A10FF)
+$(call soong_config_set,exynos_camera,target_variant,troika)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
