ff7e3c2daeaf8247152710e32af4fb2895cc4777
Nolen Johnson <johnsonnolen@gmail.com>
2026-05-21 01:27:35 -0400

    troika: rro: Rename WifiOverlay and folder containing it
    * Beautify while here.
    * Set up soong namespace to guard it.
    
    Change-Id: Ieab42e4253ef3ce459dd459437828b404dc28232

--- device.mk
+++ device.mk
@@ -9,6 +9,10 @@ $(call inherit-product, device/motorola/exynos9610-common/common.mk)
 # Inherit proprietary files
 $(call inherit-product, vendor/motorola/troika/troika-vendor.mk)
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Wi-Fi
 PRODUCT_PACKAGES += \
-    TroikaWifiOverlay
+    WifiResTarget
